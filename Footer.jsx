762da5fa9f9c4aa4870a2841eafebfd02783859f
NitishB211 <nitishbhatnagar3000@gmail.com>
2025-01-25 11:27:05 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,23 +1,43 @@
-/* This coponent is used for our footer */
-import { useNavigate } from "react-router-dom";
-function Footer(){
-  const aboutus=useNavigate
-    return (
-        <>
-     <div className="container">
-  <footer className="py-3 my-4">
-    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary"><button onClick={() => aboutus('/aboutus')}>FREE</button></a></li>
-    </ul>
-    <p className="text-center text-body-secondary">© 2025 Company</p>
-  </footer>
-</div>
-        </>
-    )
+/* This component is used for our footer */
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <>
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link px-2 text-body-secondary">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/features" className="nav-link px-2 text-body-secondary">
+                Features
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/pricing" className="nav-link px-2 text-body-secondary">
+                Pricing
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/faqs" className="nav-link px-2 text-body-secondary">
+                FAQs
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/aboutus" className="nav-link px-2 text-body-secondary">
+                About Us
+              </Link>
+            </li>
+          </ul>
+          <p className="text-center text-body-secondary"> 2025 Company</p>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
